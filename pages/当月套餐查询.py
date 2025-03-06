31d94b33ffb9b04cb148f520f05e63f28a6981d3
AvaChang21 <121758442+AvaChang21@users.noreply.github.com>
2025-03-06 11:32:09 +0800

    Update 当月套餐查询.py

--- pages/当月套餐查询.py
+++ pages/当月套餐查询.py
@@ -6,7 +6,7 @@ import io
 
 def read_sheets(df):
     data = {}
-    for name in df.sheet_names:
+    for name in df.sheet_names[:6]:
         df_pre = df.parse(sheet_name=name)
         data[name] = df_pre
 
@@ -25,31 +25,50 @@ def repack_data(data):
                      '混合浮动长协', '混合浮动月竞', '混合浮动代购', '年度长协交易均价', '月度竞价出清价', '代购价格',
                      '固定服务费', '归属', '基准价', '预估电量', '备注'])
 
+        aa = pd.DataFrame(
+            columns=['零售用户名称', '合同开始日期', '合同结束日期', '固定价格比例', '固定价格', '长协比例',
+                     '长协低于部分甲方', '长协低于部分乙方',
+                     '月竞比例', '月竞低于部分甲方', '月竞低于部分乙方', '代购比例', '代购盈利甲方占比',
+                     '代购盈利乙方占比',
+                     '混合浮动长协', '混合浮动月竞', '混合浮动代购', '年度长协交易均价', '月度竞价出清价', '代购价格',
+                     '固定服务费', '归属', '基准价', '预估电量', '备注'])
+
+        bb = pd.DataFrame(
+            columns=['零售用户名称', '合同开始日期', '合同结束日期', '固定价格比例', '固定价格', '长协比例',
+                     '长协低于部分甲方', '长协低于部分乙方',
+                     '月竞比例', '月竞低于部分甲方', '月竞低于部分乙方', '代购比例', '代购盈利甲方占比',
+                     '代购盈利乙方占比',
+                     '混合浮动长协', '混合浮动月竞', '混合浮动代购', '年度长协交易均价', '月度竞价出清价', '代购价格',
+                     '固定服务费', '归属', '基准价', '预估电量', '备注'])
+
         pattern1 = r'\d+\.\d+'
         if k == '标准套餐':
             v['固定价格电量比例'] = v['固定价格电量比例'].apply(pd.to_numeric, errors='coerce')
-            a = v[v['固定价格电量比例'] == 0]
+            a = v[v['固定价格电量比例'] == 100]
             b = v[v['固定价格电量比例'] == 0]
 
-            a['零售用户名称'] = v['零售用户名称']
-            a['合同开始日期'] = v['合同开始日期']
-            a['合同结束日期'] = v['合同结束日期']
-            a['固定价格'] = v['零售固定成交价']
+            aa['零售用户名称'] = a['零售用户名称']
+            aa['合同开始日期'] = a['合同开始日期']
+            aa['合同结束日期'] = a['合同结束日期']
+            aa['固定价格'] = a['零售固定成交价']
 
-            b['零售用户名称'] = v['零售用户名称']
-            b['合同开始日期'] = v['合同开始日期']
-            b['合同结束日期'] = v['合同结束日期']
-            b['长协比例'] = v['长协比例']
-            b['长协低于部分甲方'] = v['长协盈利甲方占比']
-            b['长协低于部分乙方'] = v['长协盈利乙方占比']
+            bb['零售用户名称'] = b['零售用户名称']
+            bb['合同开始日期'] = b['合同开始日期']
+            bb['合同结束日期'] = b['合同结束日期']
+            bb['长协比例'] = b['长协比例']
+            bb['长协低于部分甲方'] = b['长协盈利甲方占比']
+            bb['长协低于部分乙方'] = b['长协盈利乙方占比']
 
-            b['月竞比例'] = v['月竞比例']
-            b['月竞低于部分甲方'] = v['月度盈利甲方占比']
-            b['月竞低于部分乙方'] = v['月度盈利乙方占比']
+            bb['月竞比例'] = b['月竞比例']
+            bb['月竞低于部分甲方'] = b['月度盈利甲方占比']
+            bb['月竞低于部分乙方'] = b['月度盈利乙方占比']
 
-            ndf = pd.concat([a, b], ignore_index=True)
+            bb['基准价'] = b['分成基准价'].str.extract(r'(\d+\.\d+)')
 
-            ndf['基准价'] = v['分成基准价'].apply(lambda x: re.findall(pattern1, x)[0])
+            ndf = pd.concat([aa, bb], ignore_index=True)
+
+            # ndf['基准价'] = v['分成基准价'].apply(lambda x: re.findall(pattern1, x)[0])
+            # ndf['基准价'] = v['分成基准价'].str.extract(r'(\d+\.\d+)')
 
             clean_data[k] = ndf
 
@@ -100,7 +119,8 @@ def repack_data(data):
             ndf['合同开始日期'] = v['合同开始日期']
             ndf['合同结束日期'] = v['合同结束日期']
 
-            ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            # ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            ndf['基准价'] = v['分成参考价'].str.extract(r'(\d+\.\d+)')
 
             ndf['长协比例'] = v['分成长协占比']
             ndf['长协低于部分甲方'] = v['分成长协盈利甲方占比']
@@ -142,7 +162,8 @@ def repack_data(data):
             ndf['固定价格比例'] = v['固定价格类比例']
             ndf['固定价格'] = v['固定价格']
 
-            ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            # ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            ndf['基准价'] = v['分成参考价'].str.extract(r'(\d+\.\d+)')
 
             ndf['长协比例'] = v['分成长协占比']
             ndf['长协低于部分甲方'] = v['分成长协盈利甲方占比']
@@ -171,7 +192,8 @@ def repack_data(data):
             ndf['月度竞价出清价'] = v['价格浮动类月竞价格']
             ndf['代购价格'] = v['价格浮动类代购价格']
 
-            ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            # ndf['基准价'] = v['分成参考价'].apply(lambda x: re.findall(pattern1, x)[0])
+            ndf['基准价'] = v['分成参考价'].str.extract(r'(\d+\.\d+)')
 
             ndf['长协比例'] = v['分成长协占比']
             ndf['长协低于部分甲方'] = v['分成长协盈利甲方占比']
@@ -236,12 +258,14 @@ def find_date(df, year_month):
 
 uploaded_file = st.file_uploader("上传 Excel 文件", type=["xlsx", "xls"])
 
+rename_file = st.file_uploader('如有用户更名，请上传表格', type=["xlsx"])
+
 if uploaded_file:
     xls = pd.ExcelFile(uploaded_file)
     sheet_names = xls.sheet_names
 
     data = {}
-    for sheet in sheet_names[:6]:
+    for sheet in sheet_names:
         data[sheet] = pd.read_excel(xls, sheet_name=sheet)
 
     clean_data = repack_data(data)
@@ -250,8 +274,15 @@ if uploaded_file:
 
     nan_value = float("NaN")
     a.replace("", nan_value, inplace=True)
-
     a.dropna(how='all', axis=1, inplace=True)
+
+    if rename_file:
+        rn = pd.read_excel(rename_file)
+        mapping_dict = dict(zip(rn['套餐表格原称'], rn['零售用户名称']))
+        st.write(mapping_dict)
+        a['零售用户名称'] = a['零售用户名称'].map(mapping_dict).fillna(a['零售用户名称'])
+        # a['零售用户名称'] = a['零售用户名称'].replace(mapping_dict)
+
     year_month = st.text_input('请输入当前年月, e.g. 2025-03', '')
     if year_month:
         df = find_date(a, year_month)
